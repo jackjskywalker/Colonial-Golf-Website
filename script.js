f2301cf62b640e0781abf3ee56b9e469c1fb1a74
jackjskywalker <jackjskywalker@yahoo.com>
2024-12-12 16:03:31 -0500

    feat: added mobile nav

--- script.js
+++ script.js
@@ -175,3 +175,11 @@ function displayMembershipDetails() {
 
 }
 
+document.addEventListener("DOMContentLoaded", function() {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const menu = document.querySelector(".menu");
+
+    menuToggle.addEventListener("click", function() {
+        menu.classList.toggle("active");
+    });
+});
